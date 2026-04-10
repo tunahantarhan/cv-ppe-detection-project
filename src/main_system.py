75ca5f85aaefe032565f8333bfb3b404b5993b1c
tunahantarhan <tunahantarhan99@gmail.com>
2026-04-10 12:26:42 +0300

    feat: main system has been updated in order to detect multiple violations at once.

--- src/main_system.py
+++ src/main_system.py
@@ -12,9 +12,13 @@ log_file = 'outputs/violations_log.csv'
 # eğitilmiş modeli yükleme
 model = YOLO('weights/best.pt') 
 
+cooldown_time = 10
 
-cooldown_time = 5 # ihlal tespit cooldown'ı  
-last_log_time = 0
+# her ihlal türü için son log zamanını tutacak
+last_logs = {
+    "NO-Hardhat": 0,
+    "NO-Safety Vest": 0
+}
 
 print("ISG Denetim Sistemi Başlatılıyor...")
 cap = cv2.VideoCapture(0)
@@ -23,25 +27,28 @@ while cap.isOpened():
     ret, frame = cap.read()
     if not ret: break
     
-    # model kamera görüntüsünü işler, %25 güven eşiğiyle sonuç verir
+    # model tarafından kameradaki görüntünün %25 güvenle işlenmesi
     results = model(frame, conf=0.25)
     current_time = time.time()
     
-    violation_detected = False
-    violation_type = ""
+    # çoklu ihlal tespitleri için geçici liste
+    current_violations = []
 
     # ihlal tespiti
     for r in results:
         for c in r.boxes.cls:
             class_name = model.names[int(c)]
-            if class_name in ["NO-Hardhat", "NO-Safety Vest"]: 
-                violation_detected = True
-                violation_type = class_name
-                break
+            
+            if class_name in last_logs:
+                if current_time - last_logs[class_name] > cooldown_time:
+                    if class_name not in current_violations:
+                        current_violations.append(class_name)
+                        # sadece yakalanan ihlal türü için cooldown ayarı
+                        last_logs[class_name] = current_time 
 
-    # ihlal tespit edildiğinde ve cooldown süresi geçmişse log tut
-    if violation_detected and (current_time - last_log_time > cooldown_time):
+    if current_violations:
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        violation_string = "_ve_".join(current_violations)
         
         # ekran görüntüsü log
         img_name = f"outputs/screenshots/ihlal_{timestamp}.jpg"
@@ -50,13 +57,12 @@ while cap.isOpened():
         # excel/csv log
         new_log = pd.DataFrame({
             "Tarih_Saat": [datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
-            "Ihlal_Turu": [violation_type],
+            "Ihlal_Turu": [violation_string],
             "Kanit_Dosyasi": [img_name]
         })
         new_log.to_csv(log_file, mode='a', header=not os.path.exists(log_file), index=False)
         
-        print(f"!! İHLAL TESPİT EDİLDİ !!: {violation_type} | Kanıt Kaydedildi")
-        last_log_time = current_time
+        print(f"!! İHLAL TESPİT EDİLDİ !!: {violation_string} | Kanıt Kaydedildi")
 
     cv2.imshow("ISG Denetim Kamerasi", results[0].plot())
     
