3f5f664cd057300439d6adc1ffffc338f6d43e8f
tunahantarhan <tunahantarhan99@gmail.com>
2026-04-16 16:29:16 +0300

    feat: main system has been updated to work better on multiple violation detection and logging.

--- src/main_system.py
+++ src/main_system.py
@@ -5,20 +5,19 @@ import os
 from ultralytics import YOLO
 import time
 
-# çıktı klasör yolları
 os.makedirs('outputs/screenshots', exist_ok=True)
 log_file = 'outputs/violations_log.csv'
 
-# eğitilmiş modeli yükleme
 model = YOLO('weights/best.pt') 
 
-cooldown_time = 10
+collection_window = 3 # ihlal tespiti sonrası varsa ekstra ihlallerin algılanma aralığı
+cooldown_time = 10 # ihlal grubu tespiti sonrası bekleme süresi 
 
-# her ihlal türü için son log zamanını tutacak
-last_logs = {
-    "NO-Hardhat": 0,
-    "NO-Safety Vest": 0
-}
+is_collecting = False
+collection_start = 0
+gathered_violations = set() # set (küme) ile ihlal türleri her grupta unique olarak tutulur
+last_log_time = 0
+best_frame_for_log = None # fotoğraf hafızada tutulur
 
 print("ISG Denetim Sistemi Başlatılıyor...")
 cap = cv2.VideoCapture(0)
@@ -28,33 +27,52 @@ while cap.isOpened():
     if not ret: break
     
     # model tarafından kameradaki görüntünün %25 güvenle işlenmesi
-    results = model(frame, conf=0.25)
+    results = model(frame, conf=0.25)   
     current_time = time.time()
     
-    # çoklu ihlal tespitleri için geçici liste
-    current_violations = []
-
-    # ihlal tespiti
+    # == ihlal tespit, veri toplama ve loglama mantığı ==
+    
+    # ihlal oluşturan durumlar
+    TARGET_VIOLATIONS = [
+        "NO-Hardhat", 
+        "NO-Safety Vest", 
+        "NO-Mask", 
+        "NO-Gloves", 
+        "NO-Goggles", 
+        "Fall-Detected"
+    ]
+    
+    current_frame_violations = []
     for r in results:
         for c in r.boxes.cls:
             class_name = model.names[int(c)]
-            
-            if class_name in last_logs:
-                if current_time - last_logs[class_name] > cooldown_time:
-                    if class_name not in current_violations:
-                        current_violations.append(class_name)
-                        # sadece yakalanan ihlal türü için cooldown ayarı
-                        last_logs[class_name] = current_time 
+            if class_name in TARGET_VIOLATIONS: 
+                current_frame_violations.append(class_name)
 
-    if current_violations:
+    # cooldown süresi geçmiş ve frame'de ihlal tespit edilmişse veri toplama başlatılır
+    if (current_time - last_log_time > cooldown_time) and current_frame_violations:
+        if not is_collecting:
+            is_collecting = True
+            collection_start = current_time
+            gathered_violations.clear()
+            print("İhlal(ler) tespit edildi, veri toplanıyor...")
+        
+        # birden çok ihlal tespit edilmişse toplanır ve en iyi kanıt görüntüsü alınır
+        for v in current_frame_violations:
+            gathered_violations.add(v)        
+        best_frame_for_log = results[0].plot()
+
+    # loglama
+    if is_collecting and (current_time - collection_start > collection_window):
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        violation_string = "_ve_".join(current_violations)
+        violation_string = "_ve_".join(sorted(list(gathered_violations)))
         
-        # ekran görüntüsü log
+        # kanıt görüntü kaydedilir
         img_name = f"outputs/screenshots/ihlal_{timestamp}.jpg"
-        cv2.imwrite(img_name, results[0].plot())
+        if best_frame_for_log is not None:
+            cv2.imwrite(img_name, best_frame_for_log)
         
-        # excel/csv log
+        # log dosyasına yeni kayıt eklenir
         new_log = pd.DataFrame({
             "Tarih_Saat": [datetime.now().strftime("%Y-%m-%d %H:%M:%S")],
             "Ihlal_Turu": [violation_string],
